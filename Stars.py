11362ba928e91f12b65c2ef2de08264543fe2dc0
Peter Ngo <981683@learn.vsb.bc.ca>
2021-10-12 14:31:50 -0700

    hi

---
+++ Stars.py
@@ -0,0 +1,12 @@
+#Peter Ngo
+#5/27/2021
+#assignment 7
+user_response = input ("text in the amount of stars ")
+
+for i in range (int(user_response)+1):
+    for star in range (i):
+        print("* ",end="")
+    print (" ")
+    
+        
+        
